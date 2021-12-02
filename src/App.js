ae083862cbf83069a402762602d737da8e61d957
Chairat <mr.chairat_lucky@icloud.com>
2021-12-02 17:32:42 +0700

    add simple code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return <h1>Hello React</h1>
+  return (
+    <>
+      <h1>โปรแกรมบัญชี รายรับ - รายจ่าย</h1>
+      <p>บันทึกข้อมูลบัญชีในแต่ละวัน</p>
+      <ul>
+        <li>
+          ค่าเดินทาง <span>-200</span>
+        </li>
+        <li>
+          เงินเดือน <span>+20,00</span>
+        </li>
+        <li>
+          อาหาร<span>-200</span>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
