fd836fe753e1230a26481d365a85811ebc8a9abe
Chairat <mr.chairat_lucky@icloud.com>
2021-12-02 16:28:39 +0700

    deleted

--- src/components/HelloComponent.js
+++
@@ -1,5 +0,0 @@
-function HelloComponent(){
-  return <h1>Hello component from other file</h1>
-}
-
-export default HelloComponent;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//ทดลองสร้าง Component
-
-//สร้าง component แยก
-import HelloComponent from  './components/HelloComponent'
-
-//function component 
-function HellComponentFunc(){
-      return <h1>Hello Component</h1>
-}
-
-//Class component
-class  HellComponentClass extends React.Component{
-  render(){
-    return <h1>Hello Component (Class)</h1>
-  }
-}
-
-ReactDOM.render(<HelloComponent/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
