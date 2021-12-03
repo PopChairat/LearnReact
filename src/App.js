b7c1ff47ccfa342d07e8ebd6639a682ce3f71ae0
Chairat <mr.chairat_lucky@icloud.com>
2021-12-03 16:39:33 +0700

    create form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Transaction from "./components/Transaction";
+import FormComponent from "./components/FormComponent";
 
 const headerStyle = { color: "red", textAlign: "center", fontSize: "1.5rem" };
 const Title = () => <h1 style={headerStyle}>โปรแกรมบัญชี รายรับ - รายจ่าย</h1>;
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="container">
       <Title />
+      <FormComponent />
       <Transaction />
     </div>
   );
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,25 +1,22 @@
 import Item from "./Item";
-import './Transaction.css'
-import { v4 as uuidv4 } from 'uuid';
+import "./Transaction.css";
+import { v4 as uuidv4 } from "uuid";
 
 const Transaction = () => {
-  const data=[
-    {title:"เงินเดือน", amount:50000},
-    {title:"น้ำมัน", amount:3000},
-    {title:"เช่าบ้าน", amount:4000},
-    {title:"ประกัน", amount:600},
-    {title:"อื่นๆ", amount:300},
-  ]
-  
-      //map item
-      //ถ้าชื่อ Props เหมือนกัน ย่อรูปได้
+  const data = [
+    { title: "เงินเดือน", amount: 50000 },
+    { title: "น้ำมัน", amount: 3000 },
+    { title: "เช่าบ้าน", amount: 4000 },
+    { title: "อื่นๆ", amount: 300 },
+  ];
+
+  //map item
+  //ถ้าชื่อ Props เหมือนกัน ย่อรูปได้
   return (
     <ul className="item-list">
-      {
-            data.map((element)=>{
-                return <Item key={uuidv4()}  {...element}/>
-            })
-      }
+      {data.map((element) => {
+        return <Item key={uuidv4()} {...element} />;
+      })}
     </ul>
   );
 };
