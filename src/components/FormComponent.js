1395abe4d4125bd4c55241229e0e0ea15a4ab098
Chairat <mr.chairat_lucky@icloud.com>
2021-12-03 17:29:46 +0700

    Add event

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,17 +1,35 @@
+import "./FormComponent.css";
+
 const FormComponent = () => {
+  const inputTitle = (e) => {};
+  const inputAmount = (e) => {};
+  const saveItem = (e) => {
+    //don't clear value (จะไม่ refresh หน้า)
+    e.preventDefault();
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={saveItem}>
         <div className="form-control">
           <label>ชื่อรายการ</label>
-          <input type="text" placeholder="ระบุชื่อรายการ" />
+          <input
+            type="text"
+            placeholder="ระบุชื่อรายการ"
+            onChange={inputTitle}
+          />
         </div>
         <div className="form-control">
           <label>จำนวนเงิน</label>
-          <input type="number" placeholder="ระบุจำนวนเงิน" />
+          <input
+            type="number"
+            placeholder="(+ รายรับ, - รายจ่าย) บาท"
+            onChange={inputAmount}
+          />
         </div>
         <div>
-          <button type="submit">เพิ่มข้อมูล</button>
+          <button type="submit" className="btnSubmit">
+            เพิ่มข้อมูล
+          </button>
         </div>
       </form>
     </div>
