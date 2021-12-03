3e81529b616f39e5432137d841df3efbc200d9fc
Chairat <mr.chairat_lucky@icloud.com>
2021-12-03 15:49:39 +0700

    สร้าง keys โดยอาศัย Library uuid สร้างให้

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,14 @@
 import Item from "./Item";
 import './Transaction.css'
+import { v4 as uuidv4 } from 'uuid';
+
 const Transaction = () => {
   const data=[
-    {id:1 ,title:"เงินเดือน", amount:50000},
-    {id:2, title:"น้ำมัน", amount:3000},
-    {id:3,title:"เช่าบ้าน", amount:4000},
-    {id:4,title:"ประกัน", amount:600},
-    {id:5,title:"อื่นๆ", amount:300},
+    {title:"เงินเดือน", amount:50000},
+    {title:"น้ำมัน", amount:3000},
+    {title:"เช่าบ้าน", amount:4000},
+    {title:"ประกัน", amount:600},
+    {title:"อื่นๆ", amount:300},
   ]
 
       //map item
@@ -15,7 +17,7 @@ const Transaction = () => {
     <ul className="item-list">
       {
             data.map((element)=>{
-                return <Item key={element.id} {...element}/>
+                return <Item key={uuidv4()}  {...element}/>
             })
       }
     </ul>
