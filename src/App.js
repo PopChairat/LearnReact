0d2d5cb7a7cb0f0c54d9e66d5c84f41788e54619
Chairat <mr.chairat_lucky@icloud.com>
2021-12-03 11:25:25 +0700

    Nested Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import "./App.css";
+import Transaction from "./components/Transaction";
+
+const Title = () => <h1>โปรแกรมบัญชี รายรับ - รายจ่าย</h1>;
 
 function App() {
   return (
     <>
-      <h1>โปรแกรมบัญชี รายรับ - รายจ่าย</h1>
-      <p>บันทึกข้อมูลบัญชีในแต่ละวัน</p>
-      <ul>
-        <li>
-          ค่าเดินทาง <span>-200</span>
-        </li>
-        <li>
-          เงินเดือน <span>+20,00</span>
-        </li>
-        <li>
-          อาหาร<span>-200</span>
-        </li>
-      </ul>
+      <Title />
+      <Transaction />
     </>
   );
 }
