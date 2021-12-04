bd8a06a86e7bffbe17ead642c2868e0243b685e5
Chairat <mr.chairat_lucky@icloud.com>
2021-12-04 13:46:35 +0700

    Add style

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,24 @@
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
+import "./Item.css";
 // const Item = (props) => {
 // const {title, amount} = props
 
 //Destructuring
-const Item = ({title, amount}) => {
+const Item = ({ title, amount }) => {
+  const status = amount < 0 ? "expense" : "income";
+
   //output to display html
   return (
-  <li>{title}<span>{amount}</span> </li>
-  )
-}
+    <li className={status}>
+      {title}
+      <span>{amount}</span>{" "}
+    </li>
+  );
+};
 
-Item.propTypes={
-  title:PropTypes.string.isRequired,
-  amount:PropTypes.number.isRequired
-}
+Item.propTypes = {
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+};
 
 export default Item;
