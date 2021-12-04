bd2a04c48d56ffde932d34777ecf17eb1cb03eeb
Chairat <mr.chairat_lucky@icloud.com>
2021-12-05 06:34:15 +0700

    Store Reducer and Dispatch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// ReactDOM.render(<App/>, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Redux Section
+import { createStore } from "redux";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      state += action.payload;
+      break;
+    case "SUBTRACT":
+      state -= action.payload;
+      break;
+    default:
+      break;
+  }
+  return state;
+};
+//Store
+const store = createStore(reducer, 50000);
+//Listen state
+store.subscribe(() => {
+  console.log("update Store:", store.getState());
+});
+//Change value in state
+store.dispatch({
+  type: "ADD",
+  payload: 1500,
+});
+store.dispatch({
+  type: "SUBTRACT",
+  payload: 500,
+});
